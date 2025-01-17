db419b9d2f1bc620919c079f38b6deab9b7fa1d5
Andrew Patton <andrew@acusti.ca>
2025-01-17 12:14:52 -0800

    Migrate apps/site to RR v7

---
+++ apps/site/app/entry.server.tsx
@@ -0,0 +1,43 @@
+/**
+ * By default, Remix will handle generating the HTTP Response for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/file-conventions/entry.server
+ */
+
+import { isbot } from "isbot";
+import { renderToReadableStream } from "react-dom/server";
+import {
+  type AppLoadContext,
+  type EntryContext,
+  ServerRouter,
+} from "react-router";
+
+export default async function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  reactRouterContext: EntryContext,
+  _loadContext: AppLoadContext
+) {
+  const body = await renderToReadableStream(
+    <ServerRouter context={reactRouterContext} url={request.url} />,
+    {
+      onError(error: unknown) {
+        // Log streaming rendering errors from inside the shell
+        console.error(error);
+        responseStatusCode = 500;
+      },
+      signal: request.signal,
+    }
+  );
+
+  if (isbot(request.headers.get("user-agent") || "")) {
+    await body.allReady;
+  }
+
+  responseHeaders.set("Content-Type", "text/html");
+  return new Response(body, {
+    headers: responseHeaders,
+    status: responseStatusCode,
+  });
+}
--- apps/site/load-context.ts
+++ apps/site/load-context.ts
@@ -7,8 +7,22 @@ type Cloudflare = Omit<PlatformProxy<Env>, "dispose" | "caches"> & {
   caches: CacheStorage;
 };
 
-declare module "@remix-run/cloudflare" {
+declare module "react-router" {
   interface AppLoadContext {
     cloudflare: Cloudflare;
   }
+
+  // TODO: remove this once we've migrated to `Route.LoaderArgs` instead for our loaders
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface LoaderFunctionArgs {
+    context: AppLoadContext;
+  }
+
+  // TODO: remove this once we've migrated to `Route.ActionArgs` instead for our actions
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface ActionFunctionArgs {
+    context: AppLoadContext;
+  }
 }
+
+export {}; // necessary for TS to treat this as a module

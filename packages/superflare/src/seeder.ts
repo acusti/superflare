4dbe4dba73ad066076b2b1c31349090c7f06e243
Andrew Patton <andrew@acusti.ca>
2025-01-22 17:35:20 -0600

    Return seed callback to enable awaiting result
    ensures that the seed command can complete all of its async work and exit properly and makes it so that errors can be caught and logged (lines 45–47)

--- packages/superflare/src/seeder.ts
+++ packages/superflare/src/seeder.ts
@@ -3,6 +3,6 @@ import { setConfig } from "./config";
 export function seed(callback: () => void) {
   return (database: D1Database) => {
     setConfig({ database: { default: database } });
-    callback();
+    return callback();
   };
 }
